3510a803ec75d688cfd3eb5382414f19ac6689a7
Refaatx147 <ahmed14refaat7@gmail.com>
2025-02-04 08:45:55 +0200

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ThinkStep App',
+      title: 'ThinkStep ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
